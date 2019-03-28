d9945c0fc01d9b45da062420668145febd9463de
p.pairan <razerfield@me.com>
2019-03-28 11:28:40 +0100

    Funktioniert Tabelle

---
+++ src/main/java/com/example/project/model/Table.java
@@ -0,0 +1,17 @@
+package com.example.project.model;
+
+public class Table {
+
+    private String name;
+
+    public Table(){}
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
